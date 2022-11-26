f257982c7d2140b5ab6045e7f7932e76dc8a590b
osshersh <olegshershnov@gmail.com>
2022-11-27 00:39:12 +0100

    Zmieniono nazwy metod w testach dla task 6 oraz dodano testy dla wszystkich dni

--- src/test/java/task6/CalendarTest.java
+++ src/test/java/task6/CalendarTest.java
@@ -8,8 +8,26 @@ import static org.junit.jupiter.api.Assertions.*;
 class CalendarTest {
 
     @Test
-    void shouldFridayWhenNumberIs5() {
-        String day = Calendar.showNameDayOfWeek(5);
-        Assertions.assertEquals("Friday",day);
+    void shouldSayWhenNumberOfDayIsNotWeekend() {
+        String monday = Calendar.showNameDayOfWeek(1);
+        String tuesday = Calendar.showNameDayOfWeek(2);
+        String wednesday = Calendar.showNameDayOfWeek(3);
+        String thursday = Calendar.showNameDayOfWeek(4);
+        String friday = Calendar.showNameDayOfWeek(5);
+
+        Assertions.assertEquals("Monday", monday);
+        Assertions.assertEquals("Tuesday",tuesday);
+        Assertions.assertEquals("Wednesday",wednesday);
+        Assertions.assertEquals("Thursday",thursday);
+        Assertions.assertEquals("Friday",friday);
+    }
+
+    @Test
+    void shouldSayWhenNumberOfDayIsWeekend() {
+        String saturday = Calendar.showNameDayOfWeek(6);
+        String sunday = Calendar.showNameDayOfWeek(7);
+
+        Assertions.assertEquals("Weekend",saturday);
+        Assertions.assertEquals("Weekend",sunday);
     }
 }
