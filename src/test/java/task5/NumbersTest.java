bb9d9cfbcddf6a396b3fd5027934d38c856f975d
osshersh <olegshershnov@gmail.com>
2022-11-27 00:11:50 +0100

    Zmieniono nazwy testow i dodano dwa testy dla task5

--- src/test/java/task5/NumbersTest.java
+++ src/test/java/task5/NumbersTest.java
@@ -8,14 +8,23 @@ import static org.junit.jupiter.api.Assertions.*;
 class NumbersTest {
 
     @Test
-    void shouldSayThatNumber40IsEven() {
+    void shouldSayThatNumberIsEven() {
         boolean isEven = Numbers.isNumberEven(40);
         Assertions.assertTrue(isEven);
     }
-
     @Test
-    void shouldSayThatNumber45IsOdd() {
+    void shouldSayThatNumberIsNotEven(){
+        boolean isNotEven = Numbers.isNumberEven(41);
+        Assertions.assertFalse(isNotEven);
+    }
+    @Test
+    void shouldSayThatNumberIsOdd() {
         boolean isOdd = Numbers.isNumberOdd(45);
         Assertions.assertTrue(isOdd);
+    }
+    @Test
+    void shouldSayThatNumberIsNotOdd(){
+        boolean isNotOdd = Numbers.isNumberOdd(40);
+        Assertions.assertFalse(isNotOdd);
     }
 }
